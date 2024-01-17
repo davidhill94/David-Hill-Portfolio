64821298174d95f5f8157bfef447fc8ab5565ef0
davidhill94 <davidhill94@gmail.com>
2024-01-17 21:28:47 +0000

    add website stack

--- david-hill-portfolio/app/components/footer.tsx
+++ david-hill-portfolio/app/components/footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div className="w-full h-48 py-sectionH sm:py=sectionHSm xl:py=sectionHXl flex flex-col items-center justify-center gap-4 text-secondary relative">
         <hr className="absolute top-0 w-1/2 mx-1/2 text-tertiary h-4 cursor-pointer"></hr>
         <Social large />
+        <p>Website built by myself using React, NextJs, TailwindCSS</p>
         <p>&copy; {new Date().getFullYear()} Hillside Crafts. All rights reserved.</p>
     </div> 
     );
