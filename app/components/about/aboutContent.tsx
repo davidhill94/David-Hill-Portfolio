aa2106eaec172b47f331709b59306342166bcc9e
davidhill94 <davidhill94@gmail.com>
2026-01-14 07:30:23 +1100

    text change

--- app/components/about/aboutContent.tsx
+++ app/components/about/aboutContent.tsx
@@ -4,14 +4,16 @@ const AboutContent = () => {
       <p>
         Hi, I’m David Hill — a self-taught IT professional from the UK with a
         passion for technology and problem-solving. I’m exploring different
-        areas of IT, including software, web development, and cybersecurity, and
-        I’m excited to apply my skills in real-world projects.
+        areas of IT, including IT support, web development, and
+        cybersecurity. I’m excited to apply my skills to real-world projects
+        and practical challenges.
       </p>
       <p>
         I bring experience from previous roles where problem-solving, attention
-        to detail, and working under pressure were key. Now, I’m focused on
-        building a career in IT, learning every day, and taking on challenges
-        that help me grow toward a long-term goal in cybersecurity.
+        to detail, and working under pressure were essential. I’m now focused on
+        building a career in IT, learning every day, and taking on opportunities
+        that help me grow, develop strong technical foundations, and contribute
+        effectively in a professional IT environment.
       </p>
     </div>
   );
